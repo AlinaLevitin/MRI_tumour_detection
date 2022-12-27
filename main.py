13f1517237bba5ea72a7d26507329d2789bd85fa
alinag <alina.levitin123@gmail.com>
2022-12-27 13:48:32 +0200

    update the dataset

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ cwd = os.getcwd()
 
 target_dir = cwd + '\\data\\brain_tumor_dataset'
 
-# ImageProcessing.view_random_images(target_dir)
+ImageProcessing.view_random_images(target_dir)
 
 # ImageProcessing.plot_data_statistics(target_dir)
 
 # ImageProcessing.crop_all_images(target_dir)
 
-ImageProcessing.view_random_images(cwd + '\\data\\cropped_brain_tumor_dataset')
+# ImageProcessing.view_random_images(cwd + '\\data\\cropped_brain_tumor_dataset')
 
