a4268b8f988e331b717627cda1ccbd214c627bbe
alinag <alina.levitin123@gmail.com>
2022-12-28 11:28:21 +0200

    train and test model

--- CNN/__init__.py
+++ CNN/__init__.py
@@ -1,4 +1,5 @@
 from .split_data import split_data
 from .load_data import load_data
 from .build_model import build_model
+from .train_CNN import train_model
 from .plot_train_val_test import plot_train_val_test
--- main.py
+++ main.py
@@ -25,9 +25,11 @@ aug_data = cwd + '\\data\\aug_data'
 
 training_data, validation_data, test_data = CNN.load_data(aug_data)
 
-CNN.plot_train_val_test(training_data, validation_data, test_data)
+# CNN.plot_train_val_test(training_data, validation_data, test_data)
 
-CNN.build_model()
+model = CNN.build_model()
+
+CNN.train_model(model, epochs=18, training_data=training_data, validation_data=validation_data)
 
 
 
